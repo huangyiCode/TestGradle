04fbc7f9f22b788790e00ce03f1da8f91c46d7b6
huangyi <978140188@qq.com>
2016-11-15 16:32:15 +0800

    gradle commit

--- choosepic/src/main/java/com/example/mylibrary/MainActivity.java
+++ choosepic/src/main/java/com/example/mylibrary/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.administrator.testgradle;
+package com.example.mylibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.choosepic.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
---
+++ mylibrary/src/main/java/com/example/mylibrary/utils/MyTestAar.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary.utils;
+
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+
+public class MyTestAar {
+
+    public static String getMessage(){
+        return "I'm AAR";
+    }
+}
